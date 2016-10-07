db8039930fe10c264b3356041c7f606f9dda829c
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-07 15:55:15 -0700

    replaces with shoulda matchers

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,25 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "should save itself" do
-    video = Video.new(title: "monk", description: "great video")
-    video.save
-    expect(Video.first).to eq(video)
-  end
-
-  it "should belong to a category" do
-    comedies = Category.create(tag: "comedies")
-    video = Video.create(title: "monk", description: "funny", category_id: comedies.id)
-    expect(video.category).to eq(comedies)
-  end
-
-  it "does not save without a title" do
-    video = Video.new(description: "some stuff")
-    expect(video.save).to eq(false)
-  end
-
-  it "does not save without a description" do 
-    video = Video.new(title: "Title")
-    expect(video.save).to eq(false)
-  end
+  it {should belong_to(:category)}
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:description)}
 end
