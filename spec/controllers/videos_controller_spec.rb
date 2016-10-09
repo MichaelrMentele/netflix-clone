e11cf728173423e294deafa72673163dd5e0dce2
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-09 13:04:30 -0700

    added search controller tests

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,23 +2,36 @@ require 'spec_helper'
 
 describe VideosController do
   describe "GET show" do 
-    context "with authenticated user" do 
-      before do 
-        session[:user_id] = Fabricate(:user).id
-      end
-      
-      it "sets @video" do 
-        video = Fabricate(:video)
-        get :show, id: video.id
-        expect(assigns(:video)).to eq(video)
-      end
-      it "renders the show template" do 
+    it "sets @video if user authenticated" do 
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
 
-      end
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
+    end
+
+    it "redirects unauthenticated users to the sign in page" do 
+      video = Fabricate(:video)
+
+      get :show, id: video.id
+      expect(response).to redirect_to login_path
     end
   end
 
   describe "GET search" do 
+    it "sets @results if user authenticated" do 
+      session[:user_id] = Fabricate(:user).id
+      test = Fabricate(:video, title: "test")
 
+      get :search, search_term: "est"
+      expect(assigns(:results)).to eq([test])
+    end
+
+    it "it redirects to login for unauthenticated users" do 
+      test = Fabricate(:video, title: "test")
+
+      get :search, search_term: "est"
+      expect(response).to redirect_to login_path
+    end
   end
 end
