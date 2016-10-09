70c66f24116bbf77e2e28946bf811cee154340d5
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-09 13:48:04 -0700

    added tests for destroy action

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -16,42 +16,57 @@ describe SessionsController do
   end
 
   describe "POST create" do 
-    context "valid credentials" do 
-      let(:alice) { alice = Fabricate(:user) }
+    context "user exists" do 
+      let(:alice) { Fabricate(:user) }
+      context "valid credentials" do 
+        before { post :create, email: alice.email, password: alice.password }
+        
+        it "sets a user id in the session" do  
+          expect(session[:user_id]).to eq(alice.id)
+        end
+        it "redirects to home" do
+          expect(response).to redirect_to home_path
+        end
 
-      before { post :create, email: alice.email, password: alice.password }
-      
-      it "sets a user id in the session" do  
-        expect(session[:user_id]).to eq(alice.id)
-      end
-      it "redirects to home" do
-        expect(response).to redirect_to home_path
-      end
-
-      it "sets the notice" do 
-        expect(flash[:notice]).not_to be_blank 
-      end
-    end
-
-    context "invalid credentials" do 
-      let(:alice) { alice = Fabricate(:user) }
-      before { post :create }
-
-      it "sets a flash error" do 
-        expect(flash[:error]).not_to be_blank
+        it "sets the notice" do 
+          expect(flash[:notice]).not_to be_blank 
+        end
       end
 
-      it "renders the new template" do 
-        expect(response).to render_template :new
-      end
+      context "invalid credentials" do 
+        before { post :create }
 
-      it "does not put the user in the session" do 
-        expect(session[:user_to]).to be_nil
+        it "sets a flash error" do 
+          expect(flash[:error]).not_to be_blank
+        end
+
+        it "renders the new template" do 
+          expect(response).to render_template :new
+        end
+
+        it "does not put the user in the session" do 
+          expect(session[:user_to]).to be_nil
+        end
       end
     end
   end
 
   describe "POST destroy" do 
+    before do 
+      session[:user_id] = Fabricate(:user).id
+      post :destroy
+    end
 
+    it "clears the session for the user" do 
+      expect(session[:user_id]).to be_nil
+    end
+
+    it "redirects to the root path" do 
+      expect(response).to redirect_to root_path
+    end
+
+    it "sets the notice" do 
+      expect(flash[:notice]).not_to be_nil
+    end
   end
 end
