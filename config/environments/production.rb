ed20c6173299befda6c41272c64271a50c572f74
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-18 12:51:46 -0700

    Updated production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { host: 'michaels-flix.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              ENV['MAILGUN_SMTP_SERVER'],
