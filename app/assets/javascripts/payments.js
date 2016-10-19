b2de5eb3000d07bbcb5bd3ee9f45d6b44f41c4a6
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-19 10:23:16 -0700

    stripe custom form

---
+++ app/assets/javascripts/payments.js
@@ -0,0 +1,31 @@
+$(function() {
+  var $form = $('#payment-form');
+  $form.submit(function(event) {
+    $form.find('.submit').prop('disabled', true);
+    Stripe.card.createToken($form, stripeResponseHandler);
+    return false;
+  });
+});
+
+function stripeResponseHandler(status, response) {
+  // Grab the form:
+  var $form = $('#payment-form');
+
+  if (response.error) { // Problem!
+
+    // Show the errors on the form:
+    $form.find('.payment-errors').text(response.error.message);
+    $form.find('.submit').prop('disabled', false); // Re-enable submission
+
+  } else { // Token was created!
+
+    // Get the token ID:
+    var token = response.id;
+
+    // Insert the token ID into the form so it gets submitted to the server:
+    $form.append($('<input type="hidden" name="stripeToken">').val(token));
+
+    // Submit the form:
+    $form.get(0).submit();
+  }
+};
